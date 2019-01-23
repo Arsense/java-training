b600cd9f07ac95ecf45e3594e813eebd6b08ef8a
Arsense <466217395@qq.com>
2019-01-23 14:30:56 +0800

    资源添加

---
+++ base-jvm/src/main/java/com/base/java/jvm/gctest/out/gc/FinalizeEscapeGC.java
@@ -0,0 +1,51 @@
+package com.base.java.jvm.gctest.out.gc;
+
+/**
+ * 关于可达性GC的测试
+ *
+ * @author tangwei
+ * @date 2019/1/23 13:36
+ */
+public class FinalizeEscapeGC {
+
+    //用与检查对象是否存活
+    private static FinalizeEscapeGC SAVE_HOOK = null;
+
+    private void aLive() {
+        System.out.println("我仍然活着");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize 方法调用了 要被回收了");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+       SAVE_HOOK = new FinalizeEscapeGC();
+
+       SAVE_HOOK = null;
+       System.gc();
+
+       //finalize执行优先级很低 暂停以等待
+       Thread.sleep(500);
+       if (SAVE_HOOK != null) {
+           SAVE_HOOK.aLive();
+       } else {
+           System.out.println("no 我现在被回收了");
+       }
+
+        SAVE_HOOK = null;
+        System.gc();
+
+        //finalize执行优先级很低 暂停以等待
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.aLive();
+        } else {
+            System.out.println("no 我现在被回收了");
+        }
+
+    }
+}
---
+++ base-jvm/src/main/java/com/base/java/jvm/gctest/out/gc/MinorGC.java
@@ -0,0 +1,39 @@
+package com.base.java.jvm.gctest.out.gc;
+
+/**
+ * VM参数: -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+ *  测试
+ *
+ * @author tangwei
+ * @date 2019/1/23 14:18
+ */
+public class MinorGC {
+
+    private static final int _1MB = 1024*1024;
+
+    /**
+     * 新生代MinorGC
+     */
+    public static void testAllocation() {
+        byte[] allocaton1,allocaton2,allocaton3,allocaton4;
+        allocaton1 = new byte[2*_1MB];
+        allocaton2 = new byte[2*_1MB];
+        allocaton3 = new byte[2*_1MB];
+        allocaton4 = new byte[4*_1MB];  //到10MB了
+    }
+
+    /** 额外添加
+     *  -XX: PretenureSizeThreshold = 3145728
+     */
+    public static void testPretenureSizeThresOld() {
+        byte[] allocation;
+        allocation = new byte[4*_1MB];
+    }
+
+
+    public static void main(String[] args) {
+//        testAllocation();
+        testPretenureSizeThresOld(); //大对象直接进入老年代
+    }
+
+}
