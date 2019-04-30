199150d31fce047fb3b67f470a40b22d2cffca6b
Arsense <466217395@qq.com>
2019-04-30 16:11:31 +0800

    for mac

--- base-new-java/src/main/java/com/base/demo/myspring/aop/TestAop.java
+++ base-new-java/src/main/java/com/base/demo/myspring/aop/TestAop.java
@@ -1,20 +1,20 @@
-package com.base.demo.myspring.aop;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
-/**
- * @author tangwei
- * @date 2019/4/23 11:36
- */
-public class TestAop {
-
-    public static void main(String[] args) {
-        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("/base-new-java/src/main/resources/spring-config.xml");
-        TestTarget target = (TestTarget) applicationContext.getBean("testAOP");
-        target.test();
-        System.out.println("--------------------");
-        target.test2();
-
-    }
-}
+//package com.base.demo.myspring.aop;
+//
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.FileSystemXmlApplicationContext;
+//
+///**
+// * @author tangwei
+// * @date 2019/4/23 11:36
+// */
+//public class TestAop {
+//
+//    public static void main(String[] args) {
+//        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("/base-new-java/src/main/resources/spring-config.xml");
+//        TestTarget target = (TestTarget) applicationContext.getBean("testAOP");
+//        target.test();
+//        System.out.println("--------------------");
+//        target.test2();
+//
+//    }
+//}
